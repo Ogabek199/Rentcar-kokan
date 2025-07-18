bbc2d41d3c149d5388e2e6faac6bbe2fe9d8a40d
ogabek <otaxonovogabek633@gmail.com>
2025-07-18 16:17:47 +0500

    chatID rename

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -43,7 +43,7 @@ const Contact = () => {
     e.preventDefault();
 
     const token = '8070117237:AAHVkDVQLv1Zg8M_57mwk7sXwQlIDpQIk7I';
-    const chatId = '1316712488';
+    const chatId = '-1002689421547';
 
     const message = `
   🟢 Yangi xabar:
@@ -84,72 +84,72 @@ const Contact = () => {
 
   return (
     <Helmet title="Contact">
-        <CommonSection title="Aloqa" />
-        <section>
-          <Container>
-            <Row>
-              <Col lg="7" md="7">
-                <h6 className="fw-bold mb-4">Aloqa qiling</h6>
+      <CommonSection title="Aloqa" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="7" md="7">
+              <h6 className="fw-bold mb-4">Aloqa qiling</h6>
 
-                <Form onSubmit={submitHandler}>
-                  <FormGroup className="contact__form">
-                    <Input
-                      name="ism"
-                      value={formData.ism}
-                      onChange={changeHandler}
-                      placeholder="Sizning ismingiz"
-                      type="text"
-                    />
-                  </FormGroup>
-                  <FormGroup className="contact__form">
-                    <Input
-                      name="email"
-                      value={formData.email}
-                      onChange={changeHandler}
-                      placeholder="Elektron pochtangiz"
-                      type="email"
-                    />
-                  </FormGroup>
-                  <FormGroup className="contact__form">
-                    <textarea
-                      name="fikr"
-                      value={formData.fikr}
-                      onChange={changeHandler}
-                      rows="5"
-                      placeholder="Fikr qoldirish..."
-                      className="textarea"
-                    ></textarea>
-                  </FormGroup>
+              <Form onSubmit={submitHandler}>
+                <FormGroup className="contact__form">
+                  <Input
+                    name="ism"
+                    value={formData.ism}
+                    onChange={changeHandler}
+                    placeholder="Sizning ismingiz"
+                    type="text"
+                  />
+                </FormGroup>
+                <FormGroup className="contact__form">
+                  <Input
+                    name="email"
+                    value={formData.email}
+                    onChange={changeHandler}
+                    placeholder="Elektron pochtangiz"
+                    type="email"
+                  />
+                </FormGroup>
+                <FormGroup className="contact__form">
+                  <textarea
+                    name="fikr"
+                    value={formData.fikr}
+                    onChange={changeHandler}
+                    rows="5"
+                    placeholder="Fikr qoldirish..."
+                    className="textarea"
+                  ></textarea>
+                </FormGroup>
 
-                  <button className="contact__btn" type="submit">
-                    Xabar yuborish
-                  </button>
-                </Form>
-              </Col>
+                <button className="contact__btn" type="submit">
+                  Xabar yuborish
+                </button>
+              </Form>
+            </Col>
 
-              <Col lg="5" md="5">
-                <div className="contact__info">
-                  <h6 className="fw-bold">Bog'lanish uchun ma'lumot</h6>
-                  <p className="section__description mb-0">
-                    turkiston 12-uy, Qo'qon sh
-                  </p>
-                  <div className=" d-flex align-items-center gap-2">
-                    <h6 className="fs-6 mb-0">Telefon:</h6>
-                    <a href="tel:+998913238587" style={{ textDecoration: "none" }} className="section__description mb-0">+998 91 323 85 87</a>
-                  </div>
-
-                  <div className=" d-flex align-items-center gap-2">
-                    <h6 className="mb-0 fs-6">Email:</h6>
-                    <p className="section__description mb-0">
-                      otaxonovogabek633@gmail.com
-                    </p>
-                  </div>
-
+            <Col lg="5" md="5">
+              <div className="contact__info">
+                <h6 className="fw-bold">Bog'lanish uchun ma'lumot</h6>
+                <p className="section__description mb-0">
+                  turkiston 12-uy, Qo'qon sh
+                </p>
+                <div className=" d-flex align-items-center gap-2">
+                  <h6 className="fs-6 mb-0">Telefon:</h6>
+                  <a href="tel:+998913238587" style={{ textDecoration: "none" }} className="section__description mb-0">+998 91 323 85 87</a>
                 </div>
-              </Col>
-            </Row>
-          </Container>
-        </section>
+
+                <div className=" d-flex align-items-center gap-2">
+                  <h6 className="mb-0 fs-6">Email:</h6>
+                  <p className="section__description mb-0">
+                    otaxonovogabek633@gmail.com
+                  </p>
+                </div>
+
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </Helmet >
   );
 };
